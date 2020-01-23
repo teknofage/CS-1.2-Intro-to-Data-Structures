a5b62ac211af2226b0331b294d6fecdd2afa30f2
teknofage <teknofage@gmail.com>
2020-01-23 11:34:43 -0800

    Added rearrange module

---
+++ rearrange.py
@@ -0,0 +1,38 @@
+import sys
+import random 
+
+
+arguments = sys.argv
+
+def rearrange():
+    for random_arg in arguments:
+        random_arg = random.randint(0, len(sys.argv)-1)
+    print(arguments[random_arg])
+    
+rearrange()
+
+
+# filename = "/Users/Funkhauser/dev/Courses/CS-1.2/words.txt"
+
+# my_file = open(filename, "r")
+
+# lines = my_file.readlines()
+
+# random_index = randiint(0, len(lines)-1)
+# lines[1]
+
+# print(lines)
+
+
+
+# def read_words_from_input(input):
+#     with open() as f:
+#         content = f.read()
+#         content = content.split()
+#         return content
+
+# def words_printed_random(input):
+#     content = read_words_from_file(poem_file)
+#     random.shuffle(content)
+#     for i in content:
+#         print (i)
